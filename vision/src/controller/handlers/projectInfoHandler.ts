7eabb78c4d41c06bbdfc89251c7165acbde4e2ed
horim <yangfaring19@gmail.com>
2026-09-09 00:23:27 +0900

    debug: project 이름이 표시되지 않던 버그 수정

--- vision/src/controller/handlers/projectInfoHandler.ts
+++ vision/src/controller/handlers/projectInfoHandler.ts
@@ -30,6 +30,7 @@ export class ProjectInfoHandler {
 
     public async handleGitInfo(message: SidebarMessage) {
         console.log(message.command);
+        this.handle(message);
         this.gitService.initialize();
         const repo = await this.gitService.getRepositoryInfo();
         const response = {git: this.gitService.exists(), repository: repo};
