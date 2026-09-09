24836944ca2a95c4f3c20411791fb596bf32878e
horim <yangfaring19@gmail.com>
2026-09-09 15:38:37 +0900

    feat: commit SHA 비교로 현재 commit과 일치하는 briefing 불러오도록 설정

--- vision/src/controller/handlers/projectBriefHandler.ts
+++ vision/src/controller/handlers/projectBriefHandler.ts
@@ -36,8 +36,8 @@ export class ProjectBriefHandler {
         }
         const projectId = vscode.workspace.getConfiguration("vision").get<string>("projectId");
         const branch = vscode.workspace.getConfiguration("vision").get<string>("branch", 'None');
-        const commitId = vscode.workspace.getConfiguration("vision").get<string>("commitId", 'None');
-        const briefName = `Vision_brief-${commitId.slice(0,7)}.md`;
+        let commitId = vscode.workspace.getConfiguration("vision").get<string>("commitId", 'None');
+        let briefName = `Vision_brief-${commitId.slice(0,7)}.md`;
 
         if ((await vscode.workspace.fs.readDirectory(vscode.Uri.file(workspace.path))).some(([name]) => name === briefName)) {
             const briefUri = vscode.Uri.joinPath(vscode.Uri.file(workspace.path), briefName);
@@ -61,6 +61,8 @@ export class ProjectBriefHandler {
                 }
             }
             const brief = response.briefing;
+            commitId = response.commit;
+            briefName = `Vision_brief-${commitId.slice(0,7)}.md`;
             const outputUri = vscode.Uri.joinPath(
                 vscode.Uri.file(workspace.path),
                 briefName
@@ -79,6 +81,7 @@ export class ProjectBriefHandler {
     public async isBriefReady(): Promise<Boolean | undefined> {
         const projectId = vscode.workspace.getConfiguration("vision").get<string>("projectId", "");
         const branch = vscode.workspace.getConfiguration("vision").get<string>("branch", 'None');
+        const commitId = vscode.workspace.getConfiguration("vision").get<string>("commitId", "None");
         try {
             const response:any = await this.APIService.get(
                 `/index/status?project_id=${projectId}@${branch}`
@@ -88,12 +91,12 @@ export class ProjectBriefHandler {
                     return true;
                 default:
                     const error = response.briefing_error;
-                    vscode.window.showInformationMessage(`브리핑이 생성되지 않았습니다: ${error}`);
                     const response2:any = await this.APIService.get(
                         `/briefing?project_id=${projectId}@${branch}`
                     );
                     if (response2.ok) {
-                        vscode.window.showInformationMessage('이전 브리핑이 존재합니다.');
+                        const briefCommitId = response2.commit;
+                        vscode.window.showInformationMessage('서버에 이전 브리핑이 존재합니다.');
                         return true;
                     }
                     return false;
